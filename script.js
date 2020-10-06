6f83d162210be7377e09a4b03588323b56fc2db2
Innuendo416 <collinstone2291@icloud.com>
2020-10-06 13:57:19 -0400

    Project modal

--- script.js
+++ script.js
@@ -1,85 +1,117 @@
-document.addEventListener('DOMContentLoaded',function(event){
-	let pageTwoAnimating = false
-	let outroAnimated = false
-	function typeText(){
-		  // array with texts to type in typewriter
-	  var dataText = [ "Hi, Im Collin.", "Former Chef.", "Television enthusiast.", "Full Stack Web Developer"];
-	  
-	  // type one text in the typwriter
-	  // keeps calling itself until the text is finished
-	  function typeWriter(text, i, fnCallback) {
-	    // chekc if text isn't finished yet
-	    if (i < (text.length)) {
-	      // add next character to h1
-	     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+let pageTwoAnimating = false
+let outroAnimated = false
+let typeText 
 
-	      // wait for a while and call this function again for next character
+
+/*##############PAGE TWO TYPING ANIMATION##############*/
+document.addEventListener('DOMContentLoaded',function(event){
+	typeText = function(){
+	  var dataText = [ "Hi, Im Collin.", "Former Chef.", "Television enthusiast.", "Full Stack Web Developer."];
+	 
+	  function typeWriter(text, i, fnCallback) {
+
+	    if (i < (text.length)) {
+	     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
 	      setTimeout(function() {
 	        typeWriter(text, i + 1, fnCallback)
 	      }, 100);
 	    }
-	    // text finished, call callback if there is a callback function
 	    else if (typeof fnCallback == 'function') {
-	      // call callback after timeout
 	      setTimeout(fnCallback, 700);
 	    }
 	  }
-	  // start a typewriter animation for a text in the dataText array
+
 	   function StartTextAnimation(i) {
 	     if (typeof dataText[i] == 'undefined'){
 	     	pageTwoAnimating = false
 	     }
-	     // check if dataText[i] exists
+
 	    if (i < dataText[i].length) {
-	      // text exists! start typewriter animation
 	     typeWriter(dataText[i], 0, function(){
-	       // after callback (and whole text has been animated), start next text
 	       StartTextAnimation(i + 1);
 	     });
 	    }
 	  }
-	  // start the text animation
+
 	  StartTextAnimation(0);
 	}
-	const isInViewport = function(elem) {
-	    const bounding = elem.getBoundingClientRect();
-	    return (
-	        bounding.top >= 0 &&
-	        bounding.left >= 0 &&
-	        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-	        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-	    );
-	};
-	const nameLogo = document.getElementById('nameLogo')
-	const pageToken = document.getElementById('page2Token')
+
+
+/*#############PROJECT MODALS#############*/
+	const cards = document.querySelectorAll('.card')
+		cards.forEach((c, i) => {
+			c.addEventListener('click', function handleModal(){
+				const modal = document.getElementById('modal')
+				const close = document.getElementById('modalClose')
+				const textBox = document.getElementById('textBox')
+				const image = document.getElementById('modalImg')
+				const title = document.getElementById('modalTitle')
+				const modalLinks = document.getElementById('modalLinks')
+				const modalSpan = document.getElementById('modalSpan')
+				const modalP = document.getElementById('modalP')
+				modalSpan.style.display = 'block'
+				if(i === 0){
+					title.innerText = 'Behind The Bar'
+					image.src = './assets/BTB.png'
+					image.alt = 'Behind The Bar'
+					modalSpan.innerText = `- 'Kitchen Confidental meets the 21st century.' `
+					modalP.innerText = `Built with the MERN stack, this web application is for users in the Toronto restaurant industry to share the latest gossip, issues, and drama occuring 'Behind the Bar.' `
+				} else if(i === 1) {
+					title.innerText = 'Connect to Collin'
+					image.alt = 'Connect to Collin'
+					image.src = './assets/C2C.png'
+					modalSpan.innerText = ''
+					modalSpan.style.display = 'none'
+					modalP.innerText = `Built with the MERN stack, and the Socket.IO library. Bluntly put, 'Connect to Collin' allows you to do just that, connect to Collin. `
+				} else if(i === 2) {
+					title.innerText = 'Quicky Quiz'
+					image.alt = 'Quicky Quiz'
+					image.src = './assets/QQ.png'
+					modalSpan.innerText = `- 'My first project' `
+					modalP.innerText = `Utilizing the OpenTriviaDB API, this React Application is a multiplayer quiz.`
+				}
+				modal.style.display = 'grid'
+				close.addEventListener('click', function(){
+					modal.style.display = 'none'
+				})
+		})
+	})
+})
+/*#################PAGE ANIMATION HANDLERS##################*/
+const isInViewport = function(elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+document.addEventListener('scroll', function(){
 	const textBox = document.getElementsByClassName('textBox')
 	const contactBox = document.getElementById('contactBox')
-	const progressBar = document.getElementById('progressBar')
-	const card = document.getElementsByClassName('card')
+	if(isInViewport(contactBox)){
+		document.getElementById('page4Arrow').style.opacity = 0
+		document.getElementById('progressBar').style.opacity = '0'
+		const path849 = document.querySelector('.path849')
+		const path850 = document.querySelector('.path850')
+		const path851 = document.querySelector('.path851')
+		const path852 = document.querySelector('.path852')
+		path849.style.animation = 'fill849 3s linear forwards'
+		path850.style.animation = 'fill850 3s linear forwards'
+		path851.style.animation = 'fill851 3s linear forwards'
+		path852.style.animation = 'fill852 3s linear forwards'
+		setTimeout(() => {
+			contactBox.classList.add('scale-in-center')
+		}, 2500)
+	}else if(isInViewport(textBox[0]) && pageTwoAnimating === false) {
+		pageTwoAnimating = true
+		typeText()
 
-	document.addEventListener('scroll', function(){
-		if(isInViewport(contactBox)){
-			document.getElementById('page4Arrow').style.opacity = 0
-			progressBar.style.opacity = '0'
-			const path849 = document.querySelector('.path849')
-			const path850 = document.querySelector('.path850')
-			const path851 = document.querySelector('.path851')
-			const path852 = document.querySelector('.path852')
-			path849.style.animation = 'fill849 3s linear forwards'
-			path850.style.animation = 'fill850 3s linear forwards'
-			path851.style.animation = 'fill851 3s linear forwards'
-			path852.style.animation = 'fill852 3s linear forwards'
-			setTimeout(() => {
-				contactBox.classList.add('scale-in-center')
-			}, 2500)
-		}else if(isInViewport(textBox[0]) && pageTwoAnimating === false) {
-			pageTwoAnimating = true
-			typeText()
-
-		} else {
-			progressBar.style.opacity = '1'
-		}
-	})
+	} else {
+		progressBar.style.opacity = '1'
+	}
 })
 
 /*#####################PROGRESS BAR #############################*/
@@ -93,3 +125,5 @@ function progress(){
 }
 
 window.onscroll = progress
+
+
